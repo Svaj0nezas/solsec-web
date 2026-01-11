5782eb552838113a8c2b5556a04713fb857001b4
Svaj0nezas <126611305+Svaj0nezas@users.noreply.github.com>
2026-01-11 22:27:08 +0100

    form

--- app/kontaktai/page.tsx
+++ app/kontaktai/page.tsx
@@ -1,8 +1,11 @@
+import { BugReportForm } from "./components/form";
+
 export default function KontaktaiPage() {
   return (
     <div>
       <h1>Kontaktai</h1>
       <p>Čia rasite mūsų kontaktinę informaciją.</p>
+      <BugReportForm />
     </div>
   );
 }
